ee4d4d8c91c7a4f6b56fa34d164e917ab4a53294
Jhonatan Konopp <jhonnkonopp@gmail.com>
2025-07-20 15:48:57 -0300

    feat: :sparkles: added prisma requests on user service

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,26 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService, User } from '../prisma.service';
+import { Prisma } from '@photo-management/prisma';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    return this.prisma.user.create({
+      data: {
+        email: createUserDto.username,
+        name: createUserDto.name,
+        password: createUserDto.password,
+      },
+    });
   }
 
-  findAll() {
-    return `This action returns all user`;
+  findAll(): Promise<User[]> {
+    return this.prisma.user.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(username: string): Promise<User | undefined> {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        email: username,
+      },
+    });
+
+    return user ?? undefined;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  update(username: string, updateUserDto: UpdateUserDto) {
+    return this.prisma.user.update({
+      data: {
+        email: updateUserDto.username,
+        name: updateUserDto.name,
+        password: updateUserDto.password,
+      },
+      where: {
+        email: username,
+      },
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(username: string): Promise<boolean> {
+    try {
+      await this.prisma.user.delete({
+        where: {
+          email: username,
+        },
+      });
+
+      return true;
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2025' || error.code === 'P2016') {
+          throw new NotFoundException(
+            `Can't find user with username ${username}`,
+          );
+        }
+      }
+      throw error;
+    }
   }
 }

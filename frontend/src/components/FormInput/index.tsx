80b4066267e81228f294c895ed785c0ab0dd14bf
Jhonatan Konopp <jhonnkonopp@gmail.com>
2025-07-21 00:56:40 -0300

    feat: :sparkles: created form input

---
+++ frontend/src/components/FormInput/index.tsx
@@ -0,0 +1,44 @@
+import type { ReactElement } from 'react';
+import { useFormContext } from 'react-hook-form';
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '../ui/form';
+import { Input } from '../ui/input';
+
+interface FormInputProps {
+  label: string;
+  name: string;
+  description?: string;
+  placeholder?: string;
+}
+
+export function FormInput({
+  label,
+  name,
+  description,
+  placeholder,
+}: Readonly<FormInputProps>): ReactElement {
+  const { control } = useFormContext();
+
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <Input placeholder={placeholder} {...field} />
+          </FormControl>
+          {description && <FormDescription>{description}</FormDescription>}
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
